55288e0a763d8032c031fed544ac06818121c363
Polarp1 <102926701+Polarp1@users.noreply.github.com>
2022-04-12 18:58:23 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ import Bot from './Bot'
 config()
 
 
+Discord.on('message', msg => {
+  if (msg.content === '!oliy') {
+    return msg.reply('Oliy is fat');
+  }
+});
+
+
 
 const bot = new Bot()
 bot.login(process.env.DISCORD_BOT_TOKEN)
